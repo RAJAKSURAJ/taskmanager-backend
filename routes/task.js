2238d5d7de1944ca175b7b2e5c84981cb810ab36
suraj-rajak <srajak@sapat.com>
2024-07-23 16:07:41 +0530

    code

--- routes/task.js
+++ routes/task.js
@@ -1,38 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../model/Task');
+const Task = require("../model/Task");
 
 //post
-router.post('/add',async(req,res)=>{
-    const {task,date,status}=req.body;
+router.post("/add", async (req, res) => {
+  const { task, date, status } = req.body;
 
-    if(!task || !date || !status){
-        return res.status(404).json({message:"Please provide all required input"})
-    }
+  if (!task || !date || !status) {
+    return res
+      .status(404)
+      .json({ message: "Please provide all required input" });
+  }
 
-    try{
-        const newTask=new Task({task,date,status})
-        await newTask.save()
-        res.status(201).json({message:"Task added successfully", task: newTask})
-    }
-    catch(error){
-console.error(error)
-res.status(500).json({message:"Server error",error})
-
-    }
-})
+  try {
+    const newTask = new Task({ task, date, status });
+    await newTask.save();
+    res.status(201).json({ message: "Task added successfully", task: newTask });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+});
 
 //get
 
-router.get('/all',async(req,res)=>{
-    try{
-        const tasks=await Task.find();
-        res.status(200).json(tasks)
-    }
-    catch(error){
-        console.log(error);
-        res.status(500).json({ message: 'Server error', error });
-    }
-})
+router.get("/all", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error", error });
+  }
+});
 
-module.exports=router;
+//delete
+
+module.exports = router;
